0e62ffa27e592ab739950d6a29b16beb024a74fb
dCulajay2019485 <128559186+dCulajay2019485@users.noreply.github.com>
2023-04-16 20:21:29 -0600

    Add files via upload

---
+++ led_secuencial/led_secuencial.ino
@@ -0,0 +1,93 @@
+/*
+ Fundacion Kinal
+ Electronica
+Grado: Quito perito
+seccion: A
+curso: taller de electronica
+Nombre: Diego Alejandro Culajay Gonzalez
+carnet: 2019485
+*/
+#define bar1 2
+#define bar2 3
+#define bar3 4
+#define bar4 5
+#define bar5 6
+#define bar6 7
+#define bar7 8
+#define bar8 9
+#define bar9 10
+#define bar10 11
+void setup() {
+pinMode(bar1, OUTPUT);
+pinMode(bar2, OUTPUT);
+pinMode(bar3, OUTPUT);
+pinMode(bar4, OUTPUT);
+pinMode(bar5, OUTPUT);
+pinMode(bar6, OUTPUT);
+pinMode(bar7, OUTPUT);
+pinMode(bar8, OUTPUT);
+pinMode(bar9, OUTPUT);
+pinMode(bar10, OUTPUT);
+}
+void loop() {
+  digitalWrite(bar1,HIGH);
+  delay(500);
+  digitalWrite(bar1,LOW);
+  digitalWrite(bar2,HIGH);
+  delay(500);
+  digitalWrite(bar2,LOW);
+  digitalWrite(bar3,HIGH);
+  delay(500);
+  digitalWrite(bar3,LOW);
+  digitalWrite(bar4,HIGH);
+  delay(500);
+  digitalWrite(bar4,LOW);
+  digitalWrite(bar5,HIGH);
+  delay(500);
+  digitalWrite(bar5,LOW);
+  digitalWrite(bar6,HIGH);
+  delay(500);
+  digitalWrite(bar6,LOW);
+  digitalWrite(bar7,HIGH);
+  delay(500);
+  digitalWrite(bar7,LOW);
+  digitalWrite(bar8,HIGH);
+  delay(500);
+  digitalWrite(bar8,LOW);
+  digitalWrite(bar9,HIGH);
+  delay(500);
+  digitalWrite(bar9,LOW);
+  digitalWrite(bar10,HIGH);
+  delay(500);
+  digitalWrite(bar10,HIGH);
+  delay(500);
+  digitalWrite(bar10,LOW);
+  digitalWrite(bar9,HIGH);
+  delay(500);
+  digitalWrite(bar9,LOW);
+  digitalWrite(bar8,HIGH);
+  delay(500);
+  digitalWrite(bar8,LOW);
+  digitalWrite(bar7,HIGH);
+  delay(500);
+  digitalWrite(bar7,LOW);
+  digitalWrite(bar6,HIGH);
+  delay(500);
+  digitalWrite(bar6,LOW);
+  digitalWrite(bar5,HIGH);
+  delay(500);
+  digitalWrite(bar5,LOW);
+  digitalWrite(bar4,HIGH);
+  delay(500);
+  digitalWrite(bar4,LOW);
+  digitalWrite(bar3,HIGH);
+  delay(500);
+  digitalWrite(bar3,LOW);
+  digitalWrite(bar2,HIGH);
+  delay(500);
+  digitalWrite(bar2,LOW);
+  digitalWrite(bar1,HIGH);
+  delay(500);
+  digitalWrite(bar1,LOW);
+  delay(500);
+}
